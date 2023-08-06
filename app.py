f22cf37a8bcc6d4b9f263275124488daf8c47cf6
Deepak Arora <talkdeepak@gmail.com>
2023-08-06 18:58:48 +0200

    try stochastic joblib

--- app.py
+++ app.py
@@ -32,8 +32,8 @@ def predict():
     try:
         # Load pretrained model as clf. Try any one model. 
         # clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
-        # clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
-        clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
+         clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
+        # clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
     except:
         LOG.info("JSON payload: %s json_payload")
         return "Model not loaded"
