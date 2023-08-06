5a927e84a73157c1f72f1fbff85e4f4bf77ecee1
Deepak Arora <talkdeepak@gmail.com>
2023-08-06 15:41:03 +0200

    Update make_predict_azure_app.sh

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://<yourappname>.azurewebsites.net:$PORT/predict 
+     -X POST https://flask-ml-azure-webapp.azurewebsites.net:$PORT/predict 
      # TODO: Replace the <yourappname> with your application name 
