fc5b16e6a037217e953ddde6965db841b6bc34ab
Deepak Arora <talkdeepak@gmail.com>
2023-08-08 10:23:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home, master deployment new Demo 3</h3>"
+    html = "<h3>Sklearn Prediction Home, master deployment new Demo 4</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
